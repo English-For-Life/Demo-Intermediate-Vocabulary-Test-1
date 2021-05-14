22e82607c81cf45767f582aa673be086c0ebb27b
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-14 13:03:15 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Come to Turkland, and ............... all these places where history lives on.",
+        options:["discover", "inform", "invent", "memorize", "mention"],
+        answer: 0
+    },
+    {
+        q:"I'm ............... a speedy answer, i.e. within a few days.",
+        options:["excepting", "expecting", "accepting"],
+        answer: 1
+    },
+    {
+        q:"Stamp collecting is one of the world's most popular ................   .",
+        options:["customs", "institutions", "hobbies", "traditions", "usages"],
+        answer: 2
+    },
+    {
+        q:"How much do I ............... you?",
+        options:["owe", "owen to"],
+        answer: 0
+    }
+]
